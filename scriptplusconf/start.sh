8eaccb94603dee33597d716db7c4a90ea9267e08
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-02-21 19:41:45 +0530

    Update start.sh

--- scriptplusconf/start.sh
+++ scriptplusconf/start.sh
@@ -14,13 +14,15 @@ ehashtwo=$(echo "$RANDOM.$RANDOM$RANDOM$RANDOM" |shasum |base64 |tr A-Z a-z | se
 echo "$ehashone.$ehashtwo@incorporatedmail.com" >/$REPONAME/gen/email
 #echo "oopu1009@incorporatedmail.com" >/$REPONAME/gen/email
 
-echo "$ehashone.$ehashtwo@incorporatedmail.com" >/var/www/html/index1.html
-
+echo "$ehashone.$ehashtwo@incorporatedmail.com" >/var/www/html/dev.html
 
 #get mail hash
 MAILNAME=$(cat /$REPONAME/gen/email)
 curl "https://temporarymail.com/ajax/?action=requestEmailAccess&value=$MAILNAME"   -H 'Accept: */*'   -H 'Referer: https://temporarymail.com/'   -H 'X-Requested-With: XMLHttpRequest'   -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'   --compressed |sed 's@secretKey":"@\nsecurekey180@g' |grep "securekey180" |sed 's@securekey180\|"}@@g' >/$REPONAME/gen/securekey
 SECUREKEY=$(cat /$REPONAME/gen/securekey)
+
+echo $SECUREKEY >>/var/www/html/dev.html
+
 
 #get mail id
 curl "https://temporarymail.com/ajax/?action=checkInbox&value=$SECUREKEY"   -H 'Accept: */*'   -H 'Referer: https://temporarymail.com/'   -H 'X-Requested-With: XMLHttpRequest'   -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'   --compressed |grep id|sed 's/},/\n/g' |tail -1 |sed 's/,/\n/g' |grep id |sed 's@"id":"\|"@@g' >/$REPONAME/gen/getmailid
@@ -33,9 +35,11 @@ GETMAILID=$(cat /$REPONAME/gen/getmailid)
 curl "https://temporarymail.com/view/?i=$GETMAILID&width=822"   -H 'authority: temporarymail.com'   -H 'upgrade-insecure-requests: 1'   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'   -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'   -H 'sec-gpc: 1'   -H 'sec-fetch-site: same-origin'   -H 'sec-fetch-mode: navigate'   -H 'sec-fetch-dest: iframe'   -H 'referer: https://temporarymail.com/'   -H 'accept-language: en-US,en;q=0.9'   --compressed >/$REPONAME/raw/file.html
 cat file.html|sed 's|"|\n|g;s|hxxp|http|g' |grep 'hubspotlinks'|head -1 >/$REPONAME/gen/randomurl
 
+echo $GETMAILID >>/var/www/html/dev.html
+
 #SCALING RAW PY 1
 cat /$REPONAME/raw/py1 |sed "s|MAILNAME|$MAILNAME|g" |sed "s|REFERRAL|$REFERRAL|g" >/$REPONAME/gen/run1.py
-python3 /$REPONAME/gen/run1.py
+python3 /$REPONAME/gen/run1.py >>/var/www/html/dev.html
 
 #getmail (gettingmails)
 
@@ -44,10 +48,14 @@ python3 /$REPONAME/gen/run1.py
 curl "https://temporarymail.com/view/?i=$GETMAILID&width=822"   -H 'authority: temporarymail.com'   -H 'upgrade-insecure-requests: 1'   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'   -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'   -H 'sec-gpc: 1'   -H 'sec-fetch-site: same-origin'   -H 'sec-fetch-mode: navigate'   -H 'sec-fetch-dest: iframe'   -H 'referer: https://temporarymail.com/'   -H 'accept-language: en-US,en;q=0.9'   --compressed >/$REPONAME/raw/file.html
 cat /$REPONAME/raw/file.html|sed 's|"|\n|g;s|hxxp|http|g' |grep 'hubspotlinks'|head -1 >/$REPONAME/gen/randomurl
 
-RANDTURL=$(cat /$REPONAME/gen/randomurl)
+sleep 20
 
+RANDTURL=$(cat /$REPONAME/gen/randomurl)
+echo "Randurl=$RANDTURL" >>/var/www/html/dev.html
+
+#sleep 
 #SCALING RAW PY 2
 cat /$REPONAME/raw/py2 |sed "s|RANDTURL|$RANDTURL|g" >/$REPONAME/gen/run2.py
-python3 /$REPONAME/gen/run2.py
+python3 /$REPONAME/gen/run2.py >>/var/www/html/dev.html
 pkill firefox
 pkill geckodriver
