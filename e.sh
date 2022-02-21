253c9373f95cf6ed8d6430a98262b8feda4a752c
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-02-21 19:05:14 +0530

    Update e.sh

--- e.sh
+++ e.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 chmod +x /$REPONAME/scriptplusconf/start.sh
 chmod +x /$REPONAME/scriptplusconf/nginx.sh
+RPORT=$(echo $PORT)
+cat /$REPONAME/scriptplusconf/nginxconf |sed "s/THERANDOMPORT/$RPORT/g" >/etc/nginx/sites-enabled/default
+nginx -g 'daemon off;'
 supervisord -c /$REPONAME/scriptplusconf/supervisord.conf
