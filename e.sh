f3094da06cdb2c211ec290ccbde84fbc5d8519b5
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-02-21 19:10:33 +0530

    Update e.sh

--- e.sh
+++ e.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 chmod +x /$REPONAME/scriptplusconf/start.sh
 chmod +x /$REPONAME/scriptplusconf/nginx.sh
-sed -i "s|/$REPONAME|$REPONAME|g"
+sed -i "s|\$REPONAME|$REPONAME|g" /$REPONAME/scriptplusconf/supervisord.conf
 supervisord -c /$REPONAME/scriptplusconf/supervisord.conf
